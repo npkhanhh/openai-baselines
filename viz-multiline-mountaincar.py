b5d1ed17dc19f1fb4a8af96f5fc060419e048cef
Khanh Nguyen <npkhanh93@gmail.com>
2020-10-28 01:31:31 +1100

    add modify reward

--- viz-multiline-mountaincar.py
+++ viz-multiline-mountaincar.py
@@ -9,20 +9,16 @@ def running_mean(x, N):
     return (cumsum[N:] - cumsum[:-N]) / float(N)
 
 
-no_per = pu.load_results('/Users/khanh/logs/mountaincar-v0-20201015-noper')
-no_per_2 = pu.load_results('/Users/khanh/logs/mountaincar-v0-20201015-noper-trial2')
-no_per_3 = pu.load_results('/Users/khanh/logs/mountaincar-v0-20201015-noper-trial3')
-no_per_20k = pu.load_results('/Users/khanh/logs/mountaincar-v0-20201015-noper-trial2-20k')
-no_per_2_20k = pu.load_results('/Users/khanh/logs/mountaincar-v0-20201015-noper-trial3-20k')
+no_per = pu.load_results('/Users/khanh/logs/mountaincar-v0-20201025-noper-trial2')
+no_per_2 = pu.load_results('/Users/khanh/logs/mountaincar-v0-20201025-noper-trial2-20k')
 plt.xlabel('episodes')
 plt.ylabel('rewards')
-r = results_noper[0]
+r = no_per[0]
 plt.plot(running_mean(r.monitor.r.values, 100), label='dqn')
 
-r_per = results_per[0]
+r_per = no_per_2[0]
 plt.plot(running_mean(r_per.monitor.r.values, 100), label='per')
 
-r_uniform = results_closest[0]
-plt.plot(running_mean(r_uniform.monitor.r.values, 100), label='uniform')
 plt.legend()
-plt.savefig('acrobot-result.png', bbox_inches='tight')
+plt.show()
+# plt.savefig('acrobot-result.png', bbox_inches='tight')
